ebe1ce3eec10309ca9c2fc0c137e2eec04545770
OmarMahdy <omarmahdy771@gmail.com>
2022-01-14 21:53:01 +0200

    SplashScreen

--- app/src/main/java/com/example/ra3y/MainActivity.java
+++ app/src/main/java/com/example/ra3y/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.ra3y;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+private static int Splash_time=4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent nextpage=new Intent(MainActivity.this,login.class);
+                startActivity(nextpage);
+                finish();
+            }
+        },Splash_time);
     }
 }
