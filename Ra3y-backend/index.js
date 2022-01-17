ae0f15e6111a12b135ffe6bc7afd9270be3a07bb
mohammedashraftaha <mohammedashraf@aucegypt.edu>
2022-01-17 23:34:52 +0200

    [+] Bug Fix

--- Ra3y-backend/index.js
+++ Ra3y-backend/index.js
@@ -3,6 +3,8 @@
 //const fb = require("./firebase");
 const db = require("./db");
 
+const SHA256 = require("crypto-js/sha256");
+
 //const FirebaseApp = fb.FirebaseApp;
 //const FirebaseAuthentication = fb.FirebaseAuthentication;
 
@@ -39,18 +41,8 @@ extended: true
 /* ********************** */
 
 // Then implement your API normally.
-router.post("/add", function (req, res) {
-let body = req.body;
-let num1 = body.num1;
-let num2 = body.num2;
 
-let result = num1 + num2;
-
-console.log(" requested an addition on " + num1 + " and " + num2 + ".");
-
-return res.json({ result: result });
-});
-
+// registerowner HERE
 
 
 /* ********************** */
@@ -69,12 +61,12 @@ let lname = body.lname;
 let email= body.email;
 let pass = body.pass;
 // storing the has of the password
-let hashpass = crypto.createHash('sha256').update(pass).digest('hex');
+let hashpass = SHA256(pass);
 
 let phonenumber = body.phonenumber;
 
 let sqlQuery = "INSERT INTO `sitter` (`UID`, `fname`,`lname`,`email`,`pass`, `phonenumber`) VALUES('"+uid+"','"+fname+"', \
-'"+email+"', '"+hashpass+"', '"+phonenumber+"' );"
+'"+lname+"','"+email+"', '"+hashpass+"', '"+phonenumber+"' );"
 
 db.query(sqlQuery,function(err,result){
 if(err){
@@ -134,7 +126,7 @@ res.status(500).json(JSON.stringify(exception));
 // Get All owner Users
 /* ********************** */
 
-router.get("/getallsitterusers", function (req, res) {
+router.get("/getallownerusers", function (req, res) {
 
 // """ This API gets all the owner users from the database """
 sqlQuery = "SELECT * FROM owner";
@@ -163,38 +155,6 @@ res.status(500).json(JSON.stringify(exception));
 // API 4
 // Get All owner Users
 /* ********************** */
-
-router.post("/test", function (req, res) {
-
-// """ This API gets all the owner users from the database """
-// sqlQuery = "SELECT * FROM owner";
-
-// db.query(sqlQuery, function (err, result) {
-
-// if (err) {
-// res.send(err);
-// throw err;
-// } else {
-// console.log("Result", JSON.stringify(result));
-// try {
-// let data = {
-// "Status": "Success",
-// result
-// };
-// res.json(data);
-// } catch (exception) {
-// res.status(500).json(JSON.stringify(exception));
-// }
-// }
-// })
-
-let body = req.body;
-
-let num1 = body.num1;
-let num2 = body.num2;
-console.log(num1+num2);
-return res.json(num1+num2);
-});
 
 
 // Export the created router
